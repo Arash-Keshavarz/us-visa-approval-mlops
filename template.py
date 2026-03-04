004f5bf0d8f5c88e8b54639cd51d2d083bd43c1d
Arash Keshavarz <Arashkeshavarzx@gmail.com>
2026-03-04 11:26:11 +0100

    folder structure added

---
+++ template.py
@@ -0,0 +1,56 @@
+"""
+Generate Project Structure Automatically
+"""
+import os
+from pathlib import Path
+
+
+project_name = "us_visa"
+
+list_of_files = [
+    f"{project_name}/__init__.py",
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/components/data_ingestion.py",
+    f"{project_name}/components/data_validation.py",
+    f"{project_name}/components/data_transformation.py",
+    f"{project_name}/components/model_trainer.py",
+    f"{project_name}/components/model_evaluation.py",
+    f"{project_name}/components/model_pusher.py",
+    f"{project_name}/config/__init__.py",
+    f"{project_name}/config/configuration.py",
+    f"{project_name}/constants/__init__.py",
+    f"{project_name}/constants/constant.py",
+    f"{project_name}/entity/__init__.py",
+    f"{project_name}/entity/artifact_entity.py",
+    f"{project_name}/entity/config_entity.py",
+    f"{project_name}/exception/__init__.py",
+    f"{project_name}/exception/exception.py",
+    f"{project_name}/logger/__init__.py",
+    f"{project_name}/logger/logger.py",
+    f"{project_name}/pipeline/__init__.py",
+    f"{project_name}/pipeline/training_pipeline.py",
+    f"{project_name}/pipeline/prediction_pipeline.py",
+    f"{project_name}/utils/__init__.py",
+    f"{project_name}/utils/common.py",
+    "app.py",
+    "Dockerfile",
+    ".dockerignore",
+    "requirements.txt",
+    "setup.py",
+    "demo.py",
+    "config/config.yaml",
+    "config/schema.yaml",
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        print(f"Creating directory: {filedir}")
+    if not os.path.exists(filepath) or os.path.getsize(filepath) == 0:
+        with open(filepath, "w") as f:
+            pass
+        print(f"Creating file: {filepath}")
+    else:
+        print(f"{filename} already exists")
